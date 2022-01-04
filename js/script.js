b02e01ac0b090f45c5c1296ee9d8e84f3eced5f2
JoaoMarcos17 <joaomarcoscalacia@hotmail.com>
2022-01-04 08:01:42 +0000

    done

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// button to change the background
+function changeBg(newBackground, newForeground ) {
+
+    document.body.style.backgroundColor = newBackground;
+    document.body.style.color = newForeground;
+    
+    }
